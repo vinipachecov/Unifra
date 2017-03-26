cc935b6698b685bff9601ebfea2ed8a52a3eb3af
Vinicius Vieira <vinipachecov@gmail.com>
2017-03-26 17:26:54 -0300

    mudanças no tictactoe

---
+++ 2017/Mobile-Dev/tictactoe/app/src/main/java/com/example/root/tictactoe/Jogo.java
@@ -0,0 +1,136 @@
+package com.example.root.tictactoe;
+
+import android.util.Log;
+
+/**
+ * Created by root on 3/23/17.
+ */
+
+public class Jogo {
+    int [][]mat;
+
+    public Jogo(){
+        mat = new int[3][];
+        for (int i = 0; i < 3; i++) {
+            mat[i] = new int[3];
+            for (int j = 0; j < 3; j++){
+                mat[i][j] = 0;
+            }
+        }
+    }
+
+    public void addMove(boolean player1,int i, int j){
+        if(player1){
+            mat[i][j] = 1;
+        }else{
+            mat[i][j] = 2;
+        }
+    }
+
+    public String checkWin(){
+        //horizontal
+        int contp1h = 0;
+        int contp2h = 0;
+        int contp1v = 0;
+        int contp2v = 0;
+        int contp1d = 0;
+        int contp2d = 0;
+        for (int i = 0; i < 3; i++) {
+            contp1h = 0;
+            contp2h = 0;
+            for (int j = 0; j < 3; j++) {
+                //verifica horizontal
+                System.out.println("valor do contp1" + contp1h);
+                System.out.println("valor do contp2" + contp2h);
+                if(mat[i][j] == 1){
+                    contp1h +=1;
+                    contp2h = 0;
+                }
+                if(mat[i][j] == 2){
+                    contp2h +=1;
+                    contp1h = 0;
+                }
+                if(contp1h == 3){
+                    //ganhou p1
+                    return "p1 Ganhou";
+                }
+                if(contp2h == 3){
+                    //ganhou p2
+                    return "p2 Ganhou";
+                }
+                
+            }
+        }
+
+
+//        // verifica vertical
+        for (int j = 0; j < 3; j++) {
+            contp1v = 0;
+            contp2v = 0;
+            for (int i = 0; i < 3; i++) {
+                if(mat[i][j] == 1){
+                    contp1v +=1;
+                    contp2v = 0;
+                }
+                if(mat[i][j] == 2){
+                    contp2v +=1;
+                    contp1v = 0;
+                }
+                if(contp1v == 3){
+                    //ganhou p1
+                    return "p1 Ganhou";
+                }
+                if(contp2v == 3){
+                    //ganhou p2
+                    return "p2 Ganhou";
+                }
+            }
+        }
+        //verificar diagonal
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if(i == j){
+                    if(mat[i][j] == 1){
+                        contp1d +=1;
+                        contp2d = 0;
+                    }
+                    if(mat[i][j] == 2){
+                        contp2d +=1;
+                        contp1d = 0;
+                    }
+                    if(contp1d == 3){
+                        //ganhou p1
+                        return "p1 Ganhou";
+                    }
+                    if(contp2d == 3){
+                        //ganhou p2
+                        return "p2 Ganhou";
+                    }
+                }
+            }
+        }
+        contp1d = 0;
+        contp2d = 0;
+        int i , j;
+        //diagonal inversa
+        for (i = 2, j = 0; i>=0 && j<3 ; i--,j++ ){
+            if(mat[i][j] == 1){
+                contp1d +=1;
+                contp2d = 0;
+            }
+            if(mat[i][j] == 2){
+                contp2d +=1;
+                contp1d = 0;
+            }
+            if(contp1d == 3){
+                //ganhou p1
+                return "p1 Ganhou";
+            }
+            if(contp2d == 3){
+                //ganhou p2
+                return "p2 Ganhou";
+            }
+        }
+        return null;
+    }
+}
--- 2017/Mobile-Dev/tictactoe/app/src/main/java/com/example/root/tictactoe/Principal.java
+++ 2017/Mobile-Dev/tictactoe/app/src/main/java/com/example/root/tictactoe/Principal.java
@@ -2,12 +2,107 @@ package com.example.root.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
+    TextView player;
+    Button upbutton1, upbutton2, upbutton3, midbutton1,midbutton2,midbutton3, bottombutton1,
+    bottombutton2, bottombutton3;
+    Jogo jogo;
+    int jogadas = 0;
+
+    public void initializewidgets(){
+        jogo = new Jogo();
+        upbutton1 = (Button) findViewById(R.id.upbutton1);
+        upbutton2 = (Button) findViewById(R.id.upbutton2);
+        upbutton3 = (Button) findViewById(R.id.upbutton3);
+        midbutton1 = (Button) findViewById(R.id.midbutton1);
+        midbutton2 = (Button) findViewById(R.id.midbutton2);
+        midbutton3 = (Button) findViewById(R.id.midbutton3);
+        bottombutton1 = (Button) findViewById(R.id.bottombutton1);
+        bottombutton2 = (Button) findViewById(R.id.bottombutton2);
+        bottombutton3 = (Button) findViewById(R.id.bottombutton3);
+        player = (TextView) findViewById(R.id.Player);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        initializewidgets();
     }
+
+
+    public void playerturn(android.view.View v){
+        switch (player.getText().toString()){
+            case "Jogador 1":
+                findbutton(v, true);
+                break;
+            case "Jogador 2":
+                findbutton(v, false);
+                break;
+        }
+    }
+
+    public void findbutton(android.view.View v, boolean player1){
+        String vitoria;
+        switch (v.getId()){
+            case R.id.upbutton1:
+                addMarker(upbutton1,player1);
+                jogo.addMove(player1,0,0);
+                break;
+            case R.id.upbutton2:
+                addMarker(upbutton2,player1);
+                jogo.addMove(player1,0,1);
+                break;
+            case R.id.upbutton3:
+                addMarker(upbutton3,player1);
+                jogo.addMove(player1,0,2);
+                break;
+            case R.id.midbutton1:
+                addMarker(midbutton1,player1);
+                jogo.addMove(player1,1,0);
+                break;
+            case R.id.midbutton2:
+                addMarker(midbutton2,player1);
+                jogo.addMove(player1,1,1);
+                break;
+            case R.id.midbutton3:
+                addMarker(midbutton3,player1);
+                jogo.addMove(player1,1,2);
+                break;
+            case R.id.bottombutton1:
+                addMarker(bottombutton1,player1);
+                jogo.addMove(player1,2,0);
+                break;
+            case R.id.bottombutton2:
+                addMarker(bottombutton2,player1);
+                jogo.addMove(player1,2,1);
+                break;
+            case R.id.bottombutton3:
+                addMarker(bottombutton3,player1);
+                jogo.addMove(player1,2,2);
+                break;
+        }
+        vitoria = jogo.checkWin();
+        if(vitoria != null &&  vitoria == "p1 Ganhou"){
+            player.setText("p1 Ganhou");
+        }
+        else if(vitoria != null &&  vitoria == "p2 Ganhou")
+            player.setText("p2 Ganhou");
+
+    }
+
+    public void addMarker(Button btn, boolean player1){
+        if(player1){
+            btn.setText("X");
+            player.setText("Jogador 2");
+        }else{
+            btn.setText("O");
+            player.setText("Jogador 1");
+        }
+    }
+
 }
