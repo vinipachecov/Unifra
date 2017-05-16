eef16fb341542b932ef50d72519b39eb8c5a756a
vinicius <vinipachecov@gmail.com>
2017-05-15 22:01:22 -0300

    fix nacioalidades spinner

--- 2017/Mobile-Dev/RecursosDeUmaAplicacao/app/src/main/java/com/example/vinipachecov/recursosdeumaaplicacao/MainActivity.java
+++ 2017/Mobile-Dev/RecursosDeUmaAplicacao/app/src/main/java/com/example/vinipachecov/recursosdeumaaplicacao/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.vinipachecov.recursosdeumaaplicacao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> spinnerArray =  new ArrayList<String>();
+
+
+        Spinner spinnerCountShoes = (Spinner)findViewById(R.id.nacionalidadeSpinner);
+        ArrayAdapter<String> spinnerCountShoesArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.spinner));
+        spinnerCountShoes.setAdapter(spinnerCountShoesArrayAdapter);
     }
 }
